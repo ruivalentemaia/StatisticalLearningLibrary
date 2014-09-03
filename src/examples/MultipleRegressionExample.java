9e842e9b4385795cafb9fea558e17703630290a3
ruivalentemaia <ruivalentemaia@gmail.com>
2014-09-03 17:45:42 +0100

    Added Feature Normalization example.

--- src/examples/MultipleRegressionExample.java
+++ src/examples/MultipleRegressionExample.java
@@ -10,19 +10,12 @@ import multipleLinearRegression.StepwiseSelector;
 public class MultipleRegressionExample {
 
 	public static void main(String[] args) throws BiffException, IOException {
-		/*MultipleLinearRegression reg = new MultipleLinearRegression("CrimeRatePerMillionHabitantsInDifferentAmericanCities.xls");
+		MultipleLinearRegression reg = new MultipleLinearRegression("CrimeRatePerMillionHabitantsInDifferentAmericanCities.xls");
 		reg.doMultipleLinearRegression();
 		StepwiseSelector sel = new StepwiseSelector(reg, "AdjustedRSquared");
 		sel.forwardSelection();
 		reg.createAllPossibleInteractions();
 		System.out.println();
-		*/
-		
-		double[] values = {1.0, 3.5, 6.6, 7.6, 7.9, 11.4, 12.1, 12.3};
-		Feature f = new Feature(1, "x", values);
-		f.printValues();
-		f.normalizeWithinScale(1, 10);
-		f.printValues();
 	}
 
 }
